dc06fb919846c31b796d4ad4197c131217eaa313
Narcos91 <giulio.biz92@yahoo.it>
2019-05-13 16:11:44 +0200

    Rivisitazione al file app.js per migliorarare la connessione al DB

--- app.js
+++ app.js
@@ -1,19 +1,61 @@
-// Stabiliamo la connessione con il DB di Mongo
 
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// Import base routes
+const routes = require('./routes/index');
+const usersRoutes = require('./routes/users');
+
+// Database configuration
 const host = 'localhost';
-const dbName = 'dbUser';
+let dbName = 'SJ-testing-1';
+
+if (process.env.NODE_ENV === 'test') {
+  dbName = 'SJ-testing-1-test';
+}
 
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb://${host}/${dbName}`);
+mongoose.connect('mongodb://'+ host + '/' + dbName, {useNewUrlParser: true});
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', function() {
-    console.error('Connection error!')
+  console.error('Connection error!');
 });
 db.once('open', function() {
+  if (process.env.NODE_ENV !== 'test') {
     console.log('DB connection Ready');
+  }
 });
 
-var User = require('./models/model');
-applicationCache.addEventListener(3001)
-module.exports=app;
+// Init express app
+const app = express();
+
+// Setup logger and body parser
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));
+}
+app.use(bodyParser.json());
+
+// Setup static public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Setup base routes
+app.use('/', routes);
+app.use('/users', usersRoutes);
+
+// Catch 404 errors
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+      .json({message: err.message, error: err});
+});
+
+module.exports = app;
