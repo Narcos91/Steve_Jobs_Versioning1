ba5e4e2a4d42ef9a56b0d939819bb8204fc33636
Narcos91 <giulio.biz92@yahoo.it>
2019-05-13 16:53:20 +0200

    Aggiunta delle funzionalità di inserimento e visualizzazione di tutti gli utenti inseriti

---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const express = require('express');
+// eslint-disable-next-line new-cap
+const router = express.Router();
+const {check} = require('express-validator/check');
+
+const User = require('../models/users');
+
+router.get('/:id', function(req, res) {
+    User.findOne({_id: req.params.id}, function(err, user) {
+      if (err) return res.status(500).json({error: err});
+      if (!user) return res.status(404).json({message: 'Utente non trovato'});
+      res.json(user);
+    });
+  });
+  
+  router.post('/', [
+    check('name').exists(),
+    check('surname').exists(),
+    check('email').isEmail(),
+    check('age').isInt(),
+  ],
+  function(req, res) {
+    const newUser = new User(req.body);
+    newUser.save(function(err) {
+      if (err) return res.status(500).json({error: err});
+      res.status(201).json(newUser);
+    });
+  });
+
+  
+  router.get('/', function(req, res) {
+    User.find(function(err, users) {
+      if (err) return res.status(500).json({error: err});
+      res.json(users);
+    });
+  });
+
+
+  module.exports = router;
