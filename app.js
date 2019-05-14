1b137ab369e658aa70213d1810d3ac146ad2aa31
colonia-irene <colonia-irene@hotmail.it>
2019-05-14 21:36:15 +0200

    modifiche app e test

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-process.env.NODE_ENV = 'test';
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+var mongoose = require ('mongoose');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
 
 // Import base routes
 const routes = require('./routes/index');
@@ -12,11 +12,6 @@ const usersRoutes = require('./routes/users');
 const host = 'localhost';
 let dbName = 'SJ-testing-1';
 
-if (process.env.NODE_ENV === 'test') {
-  dbName = 'SJ-testing-1-test';
-}
-
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://'+ host + '/' + dbName, {useNewUrlParser: true});
 
 const db = mongoose.connection;
@@ -24,22 +19,19 @@ db.on('error', function() {
   console.error('Connection error!');
 });
 db.once('open', function() {
-  if (process.env.NODE_ENV !== 'test') {
     console.log('DB connection Ready');
-  }
+ 
 });
 
 // Init express app
-const app = express();
+var app = express();
 
 // Setup logger and body parser
-if (process.env.NODE_ENV !== 'test') {
-  app.use(morgan('dev'));
-}
+
 app.use(bodyParser.json());
 
 // Setup static public folder
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
 
 // Setup base routes
 app.use('/', routes);
@@ -58,4 +50,6 @@ app.use(function(err, req, res, next) {
       .json({message: err.message, error: err});
 });
 
+var User = require ('./models/model');
+app.listen (3001)
 module.exports = app;
--- tests/routes/index.spec.js
+++ tests/routes/index.spec.js
@@ -1,11 +1,11 @@
-const User = require('../../routes/users');
-
 const app = require('../../app');
 const chaiHttp = require('chai-http');
 const chai = require('chai');
 const expect = chai.expect;
+const User = require('../../models/model');
 
 chai.use(chaiHttp);
+
 describe('GET /', () => {
     const expectedResponse = {message: 'API Works'};
     it('Test index API payload', async () => {
@@ -16,6 +16,107 @@ describe('GET /', () => {
   });
  
 
+  
+  describe('POST /', function () {
+    it('Dovrebbe aggiungere un user', function (done) {
+    
+        let newUser = {
+            name: 'Marco',
+            surname: 'Rossi',
+            email: 'MarcoR@hotmail.com',
+            dateOfBirth: '23',
+            gender: 'uomo',
+            id:'1'
+        };
+
+        chai.request(app)
+        .post('/users')
+        .send(newUser)
+        .end(function (err, res) {
+            expect(res.status).to.equal(201);
+            expect(res.body).to.be.a('object');
+            expect(res.body).to.have.a.property('name');
+            expect(res.body).to.have.a.property('surname');
+            expect(res.body).to.have.a.property('email');
+            expect(res.body).to.have.a.property('dateOfBirth');
+            expect(res.body).to.have.a.property('gender');
+            expect(res.body.id).to.have.a.property('1');
+            done();
+        });
+  });
+});
+
+describe('PUT /', function () {
+  it('put User cercato per id', function (done) {
+        
+    let user = new User ({
+      name: 'Marco',
+      surname: 'Rossi',
+      email: 'MarcoR@hotmail.com',
+      dateOfBirth: '24-08-1204',
+      gender: 'M'
+    });
+
+    user.save(function (err, user) {
+        chai.request(app)
+        .put('/users/' + user.id)
+        .end(function (err, res) {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.be.a('object');
+            expect(res.body.id).to.equal(user.id);
+            done();
+        });
+    });
+  });
+});
+  
+describe('DELETE /', function () {
+  it("testiamo l'eliminazione di un utente ", function (done) {
+
+  const response = { message: 'Utente eliminato correttamente' };
+
+  let user = new User ({
+      name: "Pippo",
+      surname: "Pollo",
+      email: "pip@gmail.com",
+      dateOfBirth: "29",
+      gender: "uomo"
+  });
+  user.save((err, user) => {
+      chai.request(app)
+      .del('/users/' + user.id)
+      .end((err, res) => {
+          expect(res.body).to.be.deep.equal(response);
+          expect(res.status).to.equal(200);
+          done();
+      });
+    });
+  });
+});
+  
+describe('SEARCH /', function () {
+  it('cercare un utente tramite id: ', function (done) {
+  let user = new User ({
+      name: "gino",
+      surname: "bello",
+      email: "beautiful@yu.com",
+      dateOfBirth: "54",
+      gender: "uomo"
+  });
+  user.save(function (err, data) {
+      chai.request(app)
+      .get('/users/' + data.id)
+      .end(function (err, res) {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.a('object');
+          expect(res.body.id).to.equal(data.id);
+          done();
+      });
+  });
+});
+}) 
+  
+  
   describe('404 Route', () => {
     const expectedResponse = {message: 'Not Found', error: {status: 404}};
     it('Test 404 route payload', async () => {
